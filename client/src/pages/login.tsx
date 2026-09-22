d23b02d5dd2d9634c68aee75293c0ca8873b6639
CodeByDanial <70098547+danialisrar09@users.noreply.github.com>
2026-09-22 18:00:00 +0500

    Setup

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -7,6 +7,8 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { ThemedTitle } from "@refinedev/mui";
 
+import { propify } from '../assets/propify.png';
+
 import { CredentialResponse } from "../interfaces/google";
 
 // Todo: Update your Google Client ID here
@@ -27,7 +29,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: GOOGLE_CLIENT_ID,
+          client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
